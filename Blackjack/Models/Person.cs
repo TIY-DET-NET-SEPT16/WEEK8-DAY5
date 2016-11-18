45875bada31ba1dbcbec99ede8e00b9ad6a16e6a
Ronda Bergman <rondabergman@hotmail.com>
2016-11-18 10:33:11 -0500

    daily commit

---
+++ Blackjack/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blackjack.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public List<Card> Hand { get; set; }
+    }
+}
